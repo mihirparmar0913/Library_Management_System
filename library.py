8eefc44ab4a98a039507fed37ffc7a59da69a838
Mihir Parmar <mihirparmar0913@gmail.com>
2024-12-26 01:07:14 +0530

    [feature|test] - add book test cases and feature  added

--- library.py
+++ library.py
@@ -9,5 +9,21 @@ class Library:
   
     # This function is responsible for adding new book in the library. 
     def add_book(self, book):
-        self.books.append(book)  
-        return True  
+       required_fields = {"isbn", "title", "author", "publication_year"}
+       if not all(field in book for field in required_fields):
+            return False, "Missing required book information."
+       #checking all information which type  
+       if not isinstance(book["isbn"], str) or not book["isbn"]:
+            return False, "Invalid ISBN."
+       if not isinstance(book["title"], str) or not book["title"]:
+            return False, "Invalid title."
+       if not isinstance(book["author"], str) or not book["author"]:
+            return False, "Invalid author."
+       if not isinstance(book["publication_year"], int) or book["publication_year"] <= 0:
+            return False, "Invalid publication year."
+       # Here we are Checking for duplicate ISBN
+       if any(b["isbn"] == book["isbn"] for b in self.books):
+            return False, "ISBN already exists."
+       self.books.append(book)  
+       return True,"Book added successfully." 
+    
--- test_library.py
+++ test_library.py
@@ -3,23 +3,63 @@ from library import Library
 
 class TestLibrary(unittest.TestCase):
     def setUp(self):
-        pass
         self.library = Library()  
+      
+    def test_add_valid_book(self):
+          #Here we are checking test case for valid book  
+          book = {
+              "isbn": "1234567890",
+              "title": "Mystic India",
+              "author": "swami harischandra",
+              "publication_year": 1925,
+          }
+          result, message = self.library.add_book(book)
+          self.assertTrue(result)
+          self.assertEqual(message, "Book added successfully.")
+          self.assertEqual(len(self.library.books), 1)
+          self.assertEqual(self.library.books[0]["isbn"],"1234567890")
 
-    def test_add_book(self):
+        
+    def test_add_book_missing_information(self):
+            #Here we are checking test case for missing fields of book
+            book = {
+                  "isbn": "987654021",
+                  "title": "Lost World",
+                  }
+            result, message = self.library.add_book(book)
+            self.assertFalse(result)
+            self.assertEqual(message, "Missing required book information.")
+
+    #Here we are checking test case for invalid ISBN of book
+    def test_add_book_invalid_isbn(self):
+       
         book = {
-            "isbn": "1234567890",
-            "title": "The Great Gatsby",
-            "author": "F. Scott Fitzgerald",
-            "publication_year": 1925,
+            "isbn": "", 
+            "title": "Holy",
+            "author": "Scott ",
+            "publication_year": 1956,
         }
-        result = self.library.add_book(book)
-
-        # Check if the book was added successfully
-        self.assertTrue(result)  
-        # Here we are checking isbn of the book  
-        self.assertEqual(self.library.books[len(self.library.books)-1]["isbn"], "1234567890")  
-
-
-if __name__ == "__main__":
+        result, message = self.library.add_book(book)
+        self.assertFalse(result)
+        self.assertEqual(message, "Invalid ISBN.")
+    def test_duplicate_isbn(self):
+        #Here we are Adding two books with the same ISBN
+        book1 = {
+            "isbn": "2341576890",
+            "title": "Satya na prayogo",
+            "author": "Mahatma Gandhi",
+            "publication_year": 1949,
+        }
+        book2 = {
+            "isbn": "2341576890", 
+            "title": "The Great India",
+            "author": "Mahat",
+            "publication_year": 1960,
+        }
+        result1, _ = self.library.add_book(book1)
+        result2, message = self.library.add_book(book2)
+        self.assertTrue(result1)
+        self.assertFalse(result2)
+        self.assertEqual(message, "ISBN already exists.")  
+if __name__ == "__main__":  
     unittest.main()
